38c57909236e6dcc12c303f91039d1261057f4b5
zhaww <zhaww@asto-inc.com>
2018-11-05 16:08:29 +0800

    英雄联盟自动刷人机

--- venv/Include/amusement/autoMove.py
+++ venv/Include/amusement/autoMove.py
@@ -5,17 +5,32 @@ import pyautogui as pag
 import time
 import random
 
-screenWidth, screenHeight = pag.size()
-#暂停10秒 进入LOL界面
-time.sleep(5)
-# print(str(screenWidth) + ' ;' + str(screenHeight))
-while True:
-    randdomWidth = random.randint(200, screenWidth-200)
-    randomHeight = random.randint(200, screenHeight-200)
-    print(str(randdomWidth) + ' ;' + str(randomHeight))
-    pag.moveTo(randdomWidth, randomHeight, 0.5)
-    # pag.click(button='right')
-    pag.typewrite('q')
+def getRGBByPosition():
     time.sleep(5)
+    img = pag.screenshot()
+    print(img.getpixel(pag.position()))
 
-# print(pag.position())
+def getRGB(x, y):
+    img = pag.screenshot()
+    print(img.getpixel((x, y)))
+
+def getPositon():
+    time.sleep(5)
+    print(pag.position())
+
+if __name__ == '__main__':
+    screenWidth, screenHeight = pag.size()
+    # 暂停10秒 进入LOL界面
+    time.sleep(10)
+    # print(str(screenWidth) + ' ;' + str(screenHeight))q
+    while True:
+        randdomWidth = random.randint(200, screenWidth - 200)
+        randomHeight = random.randint(200, screenHeight - 200)
+        randomSleep = random.randint(50, 200)
+        randomDuration = random.randint(3, 10)
+        print(str(randdomWidth) + ' ;' + str(randomHeight) + ' ;' + str(randomSleep))
+        pag.moveTo(randdomWidth, randomHeight, randomDuration/10)
+        pag.click(button='right')
+        time.sleep(randomDuration)
+        pag.typewrite('q')
+        time.sleep(randomSleep)
