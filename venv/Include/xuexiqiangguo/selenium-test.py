224429bb1eafe5b184e150026c2f6772e9285497
zww <183078747@qq.com>
2020-08-07 17:50:14 +0800

    fix

---
+++ venv/Include/xuexiqiangguo/selenium-test.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+
+browser = webdriver.Chrome()
+browser.get('https://pc.xuexi.cn/points/my-study.html')
+browser.get('https://www.xuexi.cn/')
+driver.find_element_by_class_name("dlemail").send_keys("yoyo")
+driver.find_element_by_data_data
+time.sleep(70)  # 强制等待70秒再执行下一步
