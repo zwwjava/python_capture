96d67ba8f296bcb414fcda19c35eb244b45e2c52
zww <183078747@qq.com>
2020-08-10 17:25:36 +0800

    fix

--- venv/Include/xuexiqiangguo/selenium-test.py
+++ venv/Include/xuexiqiangguo/selenium-test.py
@@ -63,6 +63,23 @@ class XueXiUtils():
         zhongyaoLink[5].click()
         sleep(186)
 
+    def xuexishipinT(self, driver):
+        driver.get('https://www.xuexi.cn/4426aa87b0b64ac671c96379a3a8bd26/db086044562a57b441c24f2af1c8e101.html#1novbsbi47k-5')
+        sleep(2)
+        zhongyaoLink = driver.find_elements_by_css_selector('.Iuu474S1L6y5p7yalKQbW.grid-cell')
+        zhongyaoLink[0].click()
+        sleep(181)
+        zhongyaoLink[1].click()
+        sleep(182)
+        zhongyaoLink[2].click()
+        sleep(183)
+        zhongyaoLink[3].click()
+        sleep(184)
+        zhongyaoLink[4].click()
+        sleep(185)
+        zhongyaoLink[5].click()
+        sleep(186)
+
 
 if __name__ == '__main__':
 
@@ -72,9 +89,10 @@ if __name__ == '__main__':
     driver = webdriver.Chrome(r'D:\SoftWare\Python\chromedriver.exe', 0, option)
     #首页登录
     driver.get('https://pc.xuexi.cn/points/my-study.html')
-    sleep(20)
-
-    dataUtil.xuexiwenzhang(driver)
-    dataUtil.clostTab(driver)
-    dataUtil.xuexishipin(driver)
-    dataUtil.clostTab(driver)
+    sleep(36000)
+    while true:
+        dataUtil.xuexiwenzhang(driver)
+        dataUtil.clostTab(driver)
+        dataUtil.xuexishipin(driver)
+        dataUtil.clostTab(driver)
+        sleep(86400)
